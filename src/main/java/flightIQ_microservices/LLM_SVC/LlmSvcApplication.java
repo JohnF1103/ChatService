036b6c80962d4f709cf042dcf7b699b980e8b2ec
JohnF1103 <johnefoster@icloud.com>
2024-12-17 13:24:48 -0500

    first commit llm svc

---
+++ src/main/java/flightIQ_microservices/LLM_SVC/LlmSvcApplication.java
@@ -0,0 +1,13 @@
+package flightIQ_microservices.LLM_SVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flightIQ_microservices/LLM_SVC/LlmSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package flightIQ_microservices.LLM_SVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
